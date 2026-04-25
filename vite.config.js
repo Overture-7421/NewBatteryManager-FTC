1ec88b5f635b363356b2324b34744b7a113a849f
SantiQ <a01571222@itesm.mx>
2026-04-24 23:13:18 -0600

    chore: add deploy script and configure docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/NewBatteryManager-FTC/', // Set base path for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
